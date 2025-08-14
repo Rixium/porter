884df8ad8d92ef38c232a3c3e5d7d83de681f72b
Daniel Pottenger <danielpottenger@outlook.com>
2025-08-14 22:18:47 +0100

    add repository construct function

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,24 @@ fn main() {
     let cli_args: Cli = Cli::parse();
 
     match &cli_args.command {
+        Some(Command::Create(create_args)) => {
+            create_repository(&create_args.name);
+        }
         Some(Command::Add(add_args)) => {
             println!("Creating new document: {:?}", add_args.name);
         }
         _ => (),
     };
+}
+
+struct Repository {
+    repository_name: String,
+}
+
+fn create_repository(repository_name: &String) {
+    let repository = Repository {
+        repository_name: repository_name.clone(),
+    };
+
+    println!("repository {} created", repository.repository_name);
 }
