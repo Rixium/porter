ad1519ad006373d70d4219e3b9fcdf8cb71bab29
Daniel Pottenger <danielpottenger@outlook.com>
2025-08-15 18:10:26 +0100

    try to create repository folder

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use clap::Parser;
 mod commands;
 use commands::cli::{Cli, Command};
 
+const PORTER_DIRECTORY_NAME: &str = ".porter";
+
 fn main() {
     let cli_args: Cli = Cli::parse();
 
@@ -21,10 +23,32 @@ struct Repository {
     repository_name: String,
 }
 
-fn create_repository(repository_name: &String) {
+fn is_repository() -> Result<bool, std::io::Error> {
+    std::fs::exists(PORTER_DIRECTORY_NAME)
+}
+
+fn create_repository(repository_name: &String) -> () {
+    let is_repository_result = is_repository();
+    let create_repository = match is_repository_result {
+        Ok(false) => true,
+        Ok(true) => {
+            eprint!("already a porter repository");
+            false
+        }
+        Err(error) => {
+            eprint!("failed to check for repository: {}", error);
+            false
+        }
+    };
+
+    if !create_repository {
+        return;
+    }
+
     let repository = Repository {
         repository_name: repository_name.clone(),
     };
 
+    let _ = std::fs::create_dir(PORTER_DIRECTORY_NAME);
     println!("repository {} created", repository.repository_name);
 }
